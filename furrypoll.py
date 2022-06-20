528a9e242aa193165d2176434419c3598dd57c81
jmbodisch <boingloing@gmail.com>
2022-06-20 01:30:34 -0400

    make furrypoll a command even though commands arent working

--- furrypoll.py
+++ furrypoll.py
@@ -5,13 +5,12 @@ class FurryPoll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.content=='!furrypoll':
-            with open('content/furrypoll.png', 'rb') as f:
-                print('furry poll...')
-                msg = await message.channel.send(file=discord.File(f))
-                await msg.add_reaction(u"\U0001F44D")
-                await msg.add_reaction(u"\U00002665")
-                await msg.add_reaction(u"\U0001F62E")
-                await msg.add_reaction(u"\U0001F622")
+    @commands.command(name='furrypoll')
+    async def furry_poll(self, message):
+        with open('content/furrypoll.png', 'rb') as f:
+            print('furry poll...')
+            msg = await message.channel.send(file=discord.File(f))
+            await msg.add_reaction(u"\U0001F44D")
+            await msg.add_reaction(u"\U00002665")
+            await msg.add_reaction(u"\U0001F62E")
+            await msg.add_reaction(u"\U0001F622")
